b9d9ecd6148ec27772d57d36f8cc63b227d6da26
Sunpm <isunpm@qq.com>
2025-04-11 15:44:45 +0800

    perf: 标记 getQueryStringify 为废弃并修复发布流程

--- packages/url/index.ts
+++ packages/url/index.ts
@@ -1,6 +1,9 @@
+import { objectToQueryString } from '../object'
+
 /**
  * 将对象转换为URL查询字符串
  *
+ * @deprecated 请使用 objectToQueryString 函数代替，它在 object 模块中。该函数将在下一个主要版本中移除。
  * @param {Record<string, any>} params - 需要转换的对象
  * @returns {string} 转换后的查询字符串，如果有参数则以?开头
  *
@@ -27,14 +30,10 @@ export function getQueryStringify(params: Record<string, any> | null | undefined
   if (!params)
     return ''
 
-  const queryString = Object.entries(params) // 将对象转换成 [key, value] 数组
-    .map(([key, value]) => encodeURI(
-      `${key}=${typeof value === 'object' ? JSON.stringify(value) : value}`,
-    )) // 将每个数组元素转换成 key=value 字符串，需要对 value 进行 JSON 序列化和 URL 编码
-    .join('&') // 将数组用 & 符号连接成字符串
+  const queryString = objectToQueryString(params)
 
   if (queryString)
     return `?${queryString}`
 
-  return queryString
+  return ''
 }
